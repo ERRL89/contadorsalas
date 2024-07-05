ffd66e02868e663bd04d2e7de32e9d297d5ed8d7
Edson Rubio <ing.edson.rubio@outlook.com>
2024-07-04 18:11:53 -0600

    UPDATE: inicio de pagina

---
+++ index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="es-MX" prefix="og: https://ogp.me/ns#">
+
+<head>
+    <?php require "head.php"; ?>
+    <title>Salas Salazar y Asociados: Soluciones Contables y Fiscales Estratégicas</title>
+    <link rel="canonical" href="https://despachocontablesalas.com" />
+    <meta property="og:title" content="Salas Salazar y Asociados: Soluciones Contables y Fiscales Estratégicas"/>
+    <meta property="og:url" content="https://despachocontablesalas.com" />
+    <meta property="og:image" content="https://despachocontablesalas.com/images/og/og-image.png" />
+</head>
+
+<body>
+    <?php require "header.php"; ?>
+
+    <main>
+        <!-- -- TITULOS Y CAROUSEL DE IMAGENES ------- -->
+        <section class="p-3">
+            <div class="row align-items-center mb-5 carouselContainer">
+                <div class="col-md-6 mb-2 container-fluid align-items-center carouselText">
+                    <h1 class="fs-3 title2"><b>Salas Salazar & Asociados: Despacho Contable, Fiscal y Administrativo para todo México.</b></h1>
+                    <h2 class="fs-5 mb-3 title1">Optimiza tu Gestión Empresarial con Nuestros Servicios Profesionales.</h2>
+                    <span class="firstText">Con más de 20 años de experiencia, en Salas Salazar & Asociados nos especializamos en proporcionar estrategias avanzadas de optimización fiscal, defensa legal ante autoridades fiscales, y consultoría personalizada en estructuras tributarias. Nuestro compromiso es garantizar eficiencia operativa y seguridad jurídica para nuestros clientes en cada aspecto de su gestión empresarial.</span>
+                    <!-- Indicadores iniciales -->
+                    <div class="container row mt-4 mb-3">
+                        <div class="col-md-3 mb-2 containerDatos">
+                            <span class="text-center contadorNum fs-3 fw-bold colorAcil" id="negocio"></span><br>
+                            <span class="contadorLugar fs-5 fw-bold">Años de experiencia</span>
+                        </div>
+                        <div class="col-md-3 mb-2 containerDatos">
+                            <span class="contadorNum fs-3 fw-bold colorAcil" id="hogares"></span><br>
+                            <span class="contadorLugar fs-5 fw-bold">Proyectos completados</span>
+                        </div>
+                        <div class="col-md-3 mb-2 containerDatos">
+                            <span class="contadorNum fs-3 fw-bold colorAcil" id="empresas"></span><br>
+                            <span class="contadorLugar fs-5 fw-bold">Clientes satisfechos</span>
+                        </div>
+                    </div>
+                    <!-- Boton de contáctanos -->
+                    <div class="mt-4 row container-fluid btnCotizacionContainer">
+                        <a href="#kit"><button class="btnCotizacion col-sm-6 mb-2 fw-bold fs-5">¡ Contáctanos hoy mismo !</button></a>
+                    </div>
+                </div>
+                <!-- Carousel de imagenes -->
+                <div class="col-md-6 mb-2 p-2">
+                    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner imgBox">
+                            <div class="carousel-item active">
+                            <center><img src="images/carousel/1.svg" class="d-block w-100 " alt="Servicio Profesional para Contabilidad e Impuestos en México"></center>
+                            </div> 
+                            <div class="carousel-item">
+                            <center><img src="images/carousel/2.svg" class="d-block w-100 " alt="Servicio Profesional para Defensa, Planeación y Asesoría Fiscal en México"></center>
+                            </div> 
+                            <div class="carousel-item">
+                            <center><img src="images/carousel/3.svg" class="d-block w-100 " alt="Servicio Profesional para Suministro de Personal, RH y Maquila de Nómina en México"></center>
+                            </div> 
+                            <div class="carousel-item">
+                            <center><img src="images/carousel/4.svg" class="d-block w-100 " alt="Servicio Profesional para Constitución de Empresas en México"></center>
+                            </div> 
+                            <div class="carousel-item">
+                            <center><img src="images/carousel/5.svg" class="d-block w-100 " alt="Servicio Profesional para Trámite y renovación REPSE en México"></center>
+                            </div> 
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        </section>
+
+        <!-- -- IDENTIFICADORES DE NEGOCIO ------- -->
+        <div class=" p-4 bg-body-tertiary">
+            <center><h4 class="fw-bold fs-3 mb-5 firstText">¿PORQUE CONTRATAR NUESTROS SERVICIOS?</h4></center>
+            <div class="container d-flex justify-content-center align-items-center lineMargin">
+                <div class="lineSection"></div>
+            </div>
+            <!-- -- SUSCRIPCION IDENTIFICADORES OPCION 2 ------ -->
+            <section id="identificadores1">
+                    <div class="containerCards mt-5">    
+                        <!-- -- IDENTIFICADOR 1 ------ -->
+                        <div id="europa" class="container1">
+                            <div class="containerCard text-center">
+                                <img class="imgCard" src="./images/identificadores/1.svg" alt="">
+                                <div class="mt-5 containerCardText">
+                                    <p class="fs-5 firstText"><strong>Diversificación de la inversión</strong></p>
+                                    <p class="textIdentificador">Con cantidades más pequeñas, puedes diversificar tu cartera en varios proyectos inmobiliarios, reduciendo el riesgo de depender de un solo activo.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- -- IDENTIFICADOR 2 ------ -->
+                        <div id="diseno" class="container1">
+                            <div class="containerCard text-center">
+                                <img class="imgCard" src="./images/identificadores/2.svg" alt="">
+                                <div class="mt-5 containerCardText">
+                                    <p class="fs-5 firstText"><strong>Acceso a proyectos exclusivos</strong></p>
+                                    <p class="textIdentificador">Puedes invertir en proyectos que de otra manera estarían fuera del alcance de inversionistas individuales.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- -- IDENTIFICADOR 3 ------ -->
+                        <div id="diseno" class="container1">
+                            <div class="containerCard text-center">
+                                <img class="imgCard" src="./images/identificadores/3.svg" alt="">
+                                <div class="mt-5 containerCardText">
+                                    <p class="fs-5 firstText"><strong>Ingresos pasivos</strong></p>
+                                    <p class="textIdentificador">A través de la compra colectiva en bienes raíces, puedes ganar ingresos pasivos en forma de  participación en los rendimientos, lo que puede contribuir a hacer crecer tu patrimonio.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- -- IDENTIFICADOR 4 ------ -->
+                        <div id="referidos" class="container1">
+                            <div class="containerCard text-center">
+                                <img class="imgCard" src="./images/identificadores/4.svg" alt="">
+                                <div class="mt-5 containerCardText">
+                                    <p class="fs-5 firstText"><strong>Transparencia y facilidad</strong></p>
+                                    <p class="textIdentificador">Brindamos información detallada sobre los proyectos y las oportunidades, lo que facilita la toma de decisiones informadas.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- -- IDENTIFICADOR 5 ------ -->
+                        <div id="instalacion" class="container1">
+                            <div class="containerCard text-center">
+                                <img class="imgCard" src="./images/identificadores/5.svg" alt="">
+                                <div class="mt-5 containerCardText">
+                                    <p class="fs-5 firstText"><strong>Baja barrera de entrada</strong></p>
+                                    <p class="textIdentificador">Puedes comenzar con inversiones relativamente pequeñas, lo que lo hace accesible para una amplia gama de inversionistas.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </section><br><br><br>
+        </div>
+
+        <!-- -- CAROUSEL DE IMAGENES ------- -->
+        <section class="p-3">
+            <div class="row align-items-center mb-5 carouselContainer">
+                <div class="col-md-6 mb-2 p-2">
+                    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner imgBox">
+                            <div class="carousel-item active">
+                            <center><img src="images/carousel/1.png" class="d-block w-100 " alt="..."></center>
+                            </div>
+
+                            <div class="carousel-item">
+                            <center><img src="images/carousel/2.png" class="d-block w-100" alt="..."></center>
+                            </div>
+
+                            <div class="carousel-item">
+                            <center><img src="images/carousel/3.png" class="d-block w-100" alt="..."></center>
+                            </div>
+
+                            <div class="carousel-item">
+                            <center><img src="images/carousel/4.png" class="d-block w-100" alt="..."></center>
+                            </div>
+
+                            <div class="carousel-item">
+                            <center><img src="images/carousel/5.png" class="d-block w-100" alt="..."></center>
+                            </div>
+
+                            <div class="carousel-item">
+                            <center><img src="images/carousel/6.png" class="d-block w-100" alt="..."></center>
+                            </div>
+                            
+                        </div>
+
+                        
+                    </div> 
+                </div>
+                <div class="col-md-6 mb-2 container-fluid align-items-center carouselText">
+                    <span class="fs-5 mb-5 title1"><strong>Tu entrada al mundo de las inversiones</strong></span><br>
+                    <span class="fs-3 mb-5 title2"><strong>INVERSIONES INMOBILIARIAS CON POCO DINERO</strong></span><br><br>
+                    
+                    
+                    <span class="firstText mt-4">En Inver Propi, te ofrecemos la oportunidad de invertir en bienes raíces con una inversión inicial accesible. Aprovecha nuestras opciones de compra colectiva y proyectos diseñados para maximizar tus rendimientos sin necesidad de grandes sumas de dinero.</span><br><br>
+                    <span class="firstText mt-4">Empieza a construir tu futuro financiero hoy mismo, incluso con un presupuesto reducido. ¡Únete a nosotros y descubre cómo puedes obtener ingresos pasivos y aumentar tu patrimonio con inversiones inteligentes!</span><br>
+                    <div class="container row mt-4 mb-3">
+                        <div class="col-md-3 mb-2 containerDatos">
+                            <span class="text-center contadorNum fs-3 fw-bold colorAcil" id="negocio"></span><br>
+                            <span class="contadorLugar fs-5 fw-bold">Inversionistas</span>
+                        </div>
+                        <div class="col-md-3 mb-2 containerDatos">
+                            <span class="contadorNum fs-3 fw-bold colorAcil" id="hogares"></span><br>
+                            <span class="contadorLugar fs-5 fw-bold">Proyectos exitosos</span>
+                        </div>
+                        <div class="col-md-3 mb-2 containerDatos">
+                            <span class="contadorNum fs-3 fw-bold colorAcil" id="empresas"></span><br>
+                            <span class="contadorLugar fs-5 fw-bold">Tasa de reinversión</span>
+                        </div>
+                    </div>
+                    <div class="mt-4 row container-fluid btnCotizacionContainer">
+                        <a href="#kit"><button class="btnCotizacion col-sm-6 mb-2 fw-bold fs-5">¡ Contáctanos hoy mismo !</button></a>
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
+
+        
+                   
+        
+    </main>
+    
+    <?php require "footer.php"; ?>
+
+</body>
+
+</html>
+
+<script>
+    $(document).ready(function(){
+        var startNegocios = 0;
+        var endNegocios = 16;
+        var startHogares = 0;
+        var endHogares = 15;
+        var startEmpresas = 0;
+        var endEmpresas = 80;
+        var time = 50;
+
+        var intervalNegocios = setInterval(function() {
+            startNegocios += 1; // Incremento en segundos
+            if (startNegocios >= endNegocios) {
+                clearInterval(intervalNegocios);
+                startNegocios = endNegocios; // Asegurarse de que no se exceda del límite
+            }
+            $("#negocio").text("+"+startNegocios);
+        }, time);
+
+        var intervalHogares = setInterval(function() {
+            startHogares += 1; // Incremento en segundos
+            if (startHogares >= endHogares) {
+                clearInterval(intervalHogares);
+                startHogares = endHogares; // Asegurarse de que no se exceda del límite
+            }
+            $("#hogares").text("+"+startHogares);
+        }, time);
+
+        var intervalEmpresas = setInterval(function() {
+            startEmpresas += 2; // Incremento en segundos
+            if (startEmpresas >= endEmpresas) {
+                clearInterval(intervalEmpresas);
+                startEmpresas = endEmpresas; // Asegurarse de que no se exceda del límite
+            }
+            $("#empresas").text("+"+startEmpresas);
+        }, time);
+    });
+
+    
+</script>
