8d66d9ba04334760082fcb50da6ffaf2eb1925e5
Edson Rubio <ing.edson.rubio@outlook.com>
2024-09-12 18:57:20 -0600

    UPDATE: Final

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
             </div></a>
             <div class="datoscontacto2">
                 <a href="https://wa.me/5218116310073" target="_blank"><ion-icon name="logo-whatsapp"></ion-icon></a>
-                <a href="https://maps.app.goo.gl/WqUQQVeA6ZcjFWbc8"
+                <a href="https://maps.app.goo.gl/VjN9qEHaQBf2tRR27"
                     target="_blank"><ion-icon name="location-outline"></ion-icon></a>
                 <a href="mailto:contacto@despachocontablesalas.com" target="_blank"><ion-icon name="mail-outline"></ion-icon></a>
                 <a href="https://www.facebook.com/DespachoContableSalas/" target="_blank"><ion-icon name="logo-facebook"></ion-icon></a>
@@ -31,7 +31,7 @@
         </div>
 
         <div class="ubicacion">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1385.6226404561223!2d-100.30129511301666!3d25.742124108048383!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x866294f4461a6777%3A0xa20620635bd1b3d8!2sAv.%20Aztl%C3%A1n%20%236659%2C%20Col.%20Moctezuma%2C%2064240%20Monterrey%2C%20N.L.!5e0!3m2!1ses-419!2smx!4v1720589033588!5m2!1ses-419!2smx" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.3172853861506!2d-100.3505847957312!3d25.732181581592187!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86629427eaf01a95%3A0xf5937996121a33d3!2sPontevedra%20231-Interior%20L4%2C%20Nueva%20Galicia%2C%2064160%20Monterrey%2C%20N.L.!5e0!3m2!1ses-419!2smx!4v1725992496506!5m2!1ses-419!2smx" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             <a href="https://wa.me/5215588093678" target="_blank"><span class="desarrolador">Sitio desarrollado por OVIT
                     Soluciones Web</span></a>
         </div>
